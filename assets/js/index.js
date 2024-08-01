36239ae9d2495d177e1907ad749b049f61b2203c
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-08-01 19:34:38 -0300

    feat: searches and displays on the screen based on the checkbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,65 @@ const gymApiURL = 'https://test-frontend-developer.s3.amazonaws.com/data/locatio
 
 const buttonSearchGyms = document.getElementById('search-btn');
 
+// chamada para a api
 
-
-const callToGymDataApi = async () => {
+const fetchGymData = async () => {
     const responseAPI = await fetch(gymApiURL);
     const data = await responseAPI.json();
 
     return data.locations;
 }
 
-function showsResultsAcademiesFound(gymsFoundArg) {
-    const gymsFound = document.querySelector('.academy-closed span');
 
-    gymsFound.textContent = gymsFoundArg.length
+// mostra na tela o resultado de academias encontradas
+
+function displayGymsCount(gyms) {
+    const gymsCountElement = document.querySelector('.academy-closed span');
+
+    gymsCountElement.textContent = gyms.length
 }
 
-callToGymDataApi().then(response => {
-    let gymData = response
 
-    showsResultsAcademiesFound(gymData)
-});
+// filtra por academias que estão abertas
+
+function filterOpenGyms(gyms) {
+    return gyms.filter(gym => gym.opened);
+}
+
+
+
+// mostra academias abertas assim que entramos na aplicação 
+
+const showOpenGyms = async () => {
+    const gymData = await fetchGymData();
+    const openGyms = filterOpenGyms(gymData);
+    
+    displayGymsCount(openGyms);
+};
+document.addEventListener('DOMContentLoaded', showOpenGyms);
+
+
+
+// verifica o checkbox e faz a busca com base no mesmo
+
+function searchForOpenAndClosedGyms(gyms) {
+    const checkboxClosedUnited = document.getElementById('closed-united');
+
+    if (checkboxClosedUnited.checked) {
+        displayGymsCount(gyms);
+    } else {
+        const openGyms = filterOpenGyms(gyms);
+        displayGymsCount(openGyms);
+    }
+}
+
+
+
+const searchGyms = async () => {
+    const gymData = await fetchGymData();
+
+    searchForOpenAndClosedGyms(gymData)
+};
+
+
+buttonSearchGyms.addEventListener('click', searchGyms);
