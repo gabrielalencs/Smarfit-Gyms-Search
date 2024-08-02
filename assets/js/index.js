eb6731d89edf41d3271fc4f9fe29f48e2e7b9141
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-08-01 22:08:41 -0300

    refactor: Improves logic for showing cards on the screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,7 +34,7 @@ function filterOpenGyms(gyms) {
 const showOpenGyms = async () => {
     const gymData = await fetchGymData();
     const openGyms = filterOpenGyms(gymData);
-    
+
     displayGymsCount(openGyms);
 };
 document.addEventListener('DOMContentLoaded', showOpenGyms);
@@ -60,7 +60,64 @@ const searchGyms = async () => {
     const gymData = await fetchGymData();
 
     searchForOpenAndClosedGyms(gymData)
+
+    const openGyms = filterOpenGyms(gymData);
+
+
+
+    const gymCardContainer = document.querySelector('.container-gym-cards');
+    gymCardContainer.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+
+    gymData.forEach(gym => {
+        const gymCard = document.createElement('div');
+        gymCard.className = 'gym-card';
+
+        gymCard.innerHTML = `
+            <div class="card-header">
+                <span class="card-status ${gym.opened ? 'status-open' : 'status-close'}">
+                    ${gym.opened ? 'Aberto' : 'Fechado'}
+                </span>
+                <h3 class="card-title">${gym.title}</h3>
+                <p class="card-address">
+                    ${gym.content ? gym.content.replace(/<\/?[^>]+(>|$)/g, "") : gym.street}
+                </p>
+            </div>
+            <div class="card-container-icons">
+                <img src="assets/images/required-mask.png" alt="icon" class="card-icon">
+                <img src="assets/images/required-towel.png" alt="icon" class="card-icon">
+                <img src="assets/images/partial-fountain.png" alt="icon" class="card-icon">
+                <img src="assets/images/allowed-lockerroom.png" alt="icon" class="card-icon">
+            </div>
+            <div class="card-schedules">
+                <p class="card-schedules-container">
+                    <span class="card-day">Seg. à Sex.</span>
+                    <span class="card-hour">06h às 22h</span>
+                </p>
+                <p class="card-schedules-container">
+                    <span class="card-day">Sáb</span>
+                    <span class="card-hour">Fechada</span>
+                </p>
+                <p class="card-schedules-container">
+                    <span class="card-day">Dom.</span>
+                    <span class="card-hour">Fechada</span>
+                </p>
+            </div>
+        `;
+        
+        fragment.appendChild(gymCard);
+    });
+
+    gymCardContainer.appendChild(fragment)
 };
+
+
+
+
+
+
+
 
 
 buttonSearchGyms.addEventListener('click', searchGyms);
